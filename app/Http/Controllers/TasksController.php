e9c6348e520157977ba3df0b9941bb2ff6ed11a6
hikkosi <hikkosi22@gmail.com>
2019-01-08 09:13:14 +0000

    post

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -15,11 +15,19 @@ class TasksController extends Controller
      */
     public function index()
     {
-         $tasks = Task::all();
-
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
+         $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
+            
+            $data = [
+                'user' => $user,
+                'tasks' => $tasks,
+            ];
+            return view('tasks.index', $data);
+        }
+        return view('welcome');
+    
     }
     
 
@@ -47,12 +55,17 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'status' => 'required|max:10',   
+            
         ]);
         
-        $task = new Task;
-        $task->status = $request->status; 
-        $task->content = $request->content;
-        $task->save();
+        $request->user()->tasks()->create([
+
+           'status' => $request->status,
+
+           'content' => $request->content,
+
+       ]);
+        
 
         return redirect('/');
     }
@@ -119,6 +132,6 @@ class TasksController extends Controller
         $task = Task::find($id);
         $task->delete();
 
-        return redirect('/');
+        return redirect('welcome');
     }
 }
--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['task', 'user_id','content','status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
